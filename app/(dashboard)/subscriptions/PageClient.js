cb0e24be23b4c84905e40c90eb82734c7ca70c84
Krushnal Patel <krushnalpatel11@gmail.com>
2023-08-10 17:34:21 +0530

    added subscriptions list

--- app/(dashboard)/subscriptions/PageClient.js
+++ app/(dashboard)/subscriptions/PageClient.js
@@ -8,7 +8,7 @@ export default function PageClient({ subscriptions }) {
 
         {Object.keys(subscriptions).map((key, index) => {
           return (
-            <div className={`mb-6 w-1/2`}>
+            <div className={`mb-6`} style={{ width: `${100 / Object.keys(subscriptions).length}%` }} key={index}>
               <h1 className="mt-2 text-xl font-bold uppercase">{key} - {subscriptions[key].emails.length}</h1>
               <ol>
                 {subscriptions[key].emails.map((email, index) => {
